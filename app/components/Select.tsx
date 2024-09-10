00835c82f10e89ea004cdea2e4b008e7ee7ca3b0
Henrik Persson <henrikp@srcpa.gov>
2024-09-10 16:27:44 -0500

    Update select button styling

--- app/components/Select.tsx
+++ app/components/Select.tsx
@@ -6,7 +6,7 @@ import { cn } from "~/lib/cn"
 export default ({placeholder, ariaLabel}: {placeholder: string, ariaLabel: string}) => {
 	return (
 		<SelectRoot>
-			<SelectTrigger className="inline-flex items-center justify-between rounded px-2 text-base leading-none h-7 gap-2 bg-white dark:bg-slate-800 hover:bg-slate-100 dark:hover:bg-slate-900 transition-colors duration-300 border border-slate-400 data-[placeholder]:text-slate-700 dark:data-[placeholder]:text-slate-300">
+			<SelectTrigger className="inline-flex items-center justify-between rounded-md px-3 py-2 text-base leading-none gap-2 bg-white dark:bg-slate-800 hover:bg-slate-100 dark:hover:bg-slate-900 transition-colors duration-300 border border-slate-400 data-[placeholder]:text-slate-700 dark:data-[placeholder]:text-slate-300">
 				<SelectValue placeholder={placeholder} aria-label={ariaLabel} />
 				<SelectIcon>
 					<FaSort size={12} />
