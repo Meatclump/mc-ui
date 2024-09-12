2d3d25980fbb3057a1e4d9c343ef9baa6c0a7206
Henrik Persson <henrikp@srcpa.gov>
2024-09-12 12:00:59 -0500

    Adjusted checkbox styles

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -9,11 +9,11 @@ const Checkbox = React.forwardRef<React.ElementRef<typeof rxCheckbox.Root>, Reac
 			ref={forwardedRef}
 			{...props}
 			className={cn(
-				"h-5 w-5 border border-slate-400 dark:border-slate-500 bg-slate-200 hover:bg-slate-300 data-[state=checked]:bg-slate-400 data-[state=checked]:hover:bg-slate-300 dark:bg-slate-800 dark:hover:data-[state=unchecked]:bg-slate-900 dark:hover:data-[state=checked]:bg-slate-600 dark:data-[state=checked]:bg-slate-500 transition-colors rounded disabled:cursor-not-allowed shrink-0 disabled:opacity-50 disabled:pointer-events-none outline-none focus-within:ring-2 ring-offset-1 ring-offset-slate-300 dark:ring-offset-slate-600 ring-indigo-200 dark:ring-indigo-950",
+				"group h-5 w-5 border border-slate-400 dark:border-slate-500 bg-white hover:bg-slate-100 data-[state=checked]:bg-slate-400 data-[state=checked]:hover:bg-slate-500 dark:bg-slate-800 dark:hover:data-[state=unchecked]:bg-slate-900 dark:hover:data-[state=checked]:bg-slate-600 dark:data-[state=checked]:bg-slate-500 transition-colors rounded disabled:cursor-not-allowed shrink-0 disabled:opacity-50 disabled:pointer-events-none outline-none focus-within:ring-2 ring-offset-1 ring-offset-slate-300 dark:ring-offset-slate-600 ring-indigo-200 dark:ring-indigo-950",
 				className
 			)}
 		>
-			<rxCheckbox.Indicator className={cn("flex items-center justify-center data-[state=checked]:text-slate-600 dark:data-[state=checked]:text-slate-800")}>
+			<rxCheckbox.Indicator className={cn("flex items-center justify-center text-white dark:data-[state=checked]:text-slate-800")}>
 				<FaCheck className="w-4 h-4" />
 			</rxCheckbox.Indicator>
 		</rxCheckbox.Root>
