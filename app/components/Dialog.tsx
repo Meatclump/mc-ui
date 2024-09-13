404ec24da23cab49866ff23fd49696797bafe0c4
Henrik Persson <henrikp@srcpa.gov>
2024-09-13 12:32:15 -0500

    Dialog

--- app/components/Dialog.tsx
+++ app/components/Dialog.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import * as rxDialog from "@radix-ui/react-dialog"
 import { cn } from "~/lib/cn"
+import { FaXmark } from "react-icons/fa6"
 
 const Dialog = rxDialog.Root
 const DialogTrigger = rxDialog.Trigger
@@ -14,7 +15,12 @@ const DialogOverlay = React.forwardRef<React.ElementRef<typeof rxDialog.Overlay>
 	return (
 		<rxDialog.Overlay
 			ref={forwardedRef}
-			className={cn("", className)}
+			className={cn(
+				"data-[state=open]:animate-in data-[state=open]:fade-in-0",
+				"data-[state=closed]:animate-out data-[state=closed]:fade-out-0",
+				"bg-slate-950/80 fixed inset-0",
+				className
+			)}
 			{...props}
 		/>
 	)
@@ -28,45 +34,22 @@ const DialogContent = React.forwardRef<React.ElementRef<typeof rxDialog.Content>
 	return (
 		<rxDialog.Content
 			ref={forwardedRef}
-			className={cn("", className)}
+			className={cn(
+				"data-[state=open]:animate-in data-[state=open]:zoom-in-95 data-[state=open]:fade-in-0 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-3",
+				"data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=closed]:fade-out-0 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-3",
+				"fixed z-50 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] max-h-[85vh] w-[90vw] max-w-[450px] rounded-md dark:bg-slate-800 dark:text-slate-300 px-4 py-6",
+				className
+			)}
 			{...props}
 		>
 			{children}
+			<rxDialog.Close className="group p-1 rounded-md absolute top-3 right-3 outline-none focus-within:ring-2 ring-offset-1 ring-indigo-200 dark:ring-indigo-950 ring-offset-slate-400">
+				<FaXmark className="text-slate-500 group-hover:text-slate-400" />
+				<span className="sr-only">Close</span>
+			</rxDialog.Close>
 		</rxDialog.Content>
 	)
 })
-
-// const DialogHeader = React.forwardRef<React.ElementRef<typeof rxDialog.Header>, React.ComponentPropsWithoutRef<typeof rxDialog.Header>>(({
-// 	children,
-// 	className,
-// 	...props
-// }, forwardedRef) => {
-// 	return (
-// 		<rxDialog.Header
-// 			ref={forwardedRef}
-// 			className={cn("", className)}
-// 			{...props}
-// 		>
-// 			{children}
-// 		</rxDialog.Header>
-// 	)
-// })
-
-// const DialogFooter = React.forwardRef<React.ElementRef<typeof rxDialog.Footer>, React.ComponentPropsWithoutRef<typeof rxDialog.Footer>>(({
-// 	children,
-// 	className,
-// 	...props
-// }, forwardedRef) => {
-// 	return (
-// 		<rxDialog.Footer
-// 			ref={forwardedRef}
-// 			className={cn("", className)}
-// 			{...props}
-// 		>
-// 			{children}
-// 		</rxDialog.Footer>
-// 	)
-// })
 
 const DialogTitle = React.forwardRef<React.ElementRef<typeof rxDialog.Title>, React.ComponentPropsWithoutRef<typeof rxDialog.Title>>(({
 	children,
@@ -76,7 +59,7 @@ const DialogTitle = React.forwardRef<React.ElementRef<typeof rxDialog.Title>, Re
 	return (
 		<rxDialog.Title
 			ref={forwardedRef}
-			className={cn("", className)}
+			className={cn("text-xl leading-none tracking-normal dark:text-slate-300", className)}
 			{...props}
 		>
 			{children}
@@ -92,7 +75,7 @@ const DialogDescription = React.forwardRef<React.ElementRef<typeof rxDialog.Desc
 	return (
 		<rxDialog.Description
 			ref={forwardedRef}
-			className={cn("", className)}
+			className={cn("text-sm dark:text-slate-400", className)}
 			{...props}
 		>
 			{children}
@@ -106,5 +89,7 @@ export {
 	DialogPortal,
 	DialogClose,
 	DialogOverlay,
-	DialogContent
+	DialogContent,
+	DialogTitle,
+	DialogDescription,
 }
--- app/components/demoCards/DialogCard.tsx
+++ app/components/demoCards/DialogCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { CardContent, CardDescription, CardHeader, CardTitle, CardWrapper } from "../Card"
-import {Switch} from "../Switch"
+import { Dialog, DialogContent, DialogOverlay, DialogPortal, DialogTitle, DialogDescription, DialogTrigger, DialogClose } from "../Dialog"
+import { Button } from "../Button"
 
 const DialogCard = () => {
 	const [checked, setChecked] = useState(false)
@@ -15,7 +16,31 @@ const DialogCard = () => {
 				</CardDescription>
 			</CardHeader>
 			<CardContent className="min-h-[200px] max-h-[300px] items-center justify-center">
-				
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button>
+							Sample Dialog
+						</Button>
+					</DialogTrigger>
+					<DialogPortal>
+						<DialogOverlay />
+						<DialogContent>
+							<DialogTitle>
+								Sample Dialog
+							</DialogTitle>
+							<DialogDescription>
+								This is a sample description for the dialog.
+							</DialogDescription>
+							<div className="pt-3 flex justify-end">
+								<DialogClose asChild>
+									<Button variant="contrast" className="focus:outline-slate-400 outline-1 focus-within:ring-2 ring-offset-1 ring-indigo-200 dark:ring-indigo-950 ring-offset-transparent">
+										Close
+									</Button>
+								</DialogClose>
+							</div>
+						</DialogContent>
+					</DialogPortal>
+				</Dialog>
 			</CardContent>
 		</CardWrapper>
 	)
