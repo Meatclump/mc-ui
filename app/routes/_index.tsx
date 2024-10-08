c648f559fc61f8dc21f5c1946887a8960ce40066
Henrik Persson <henrikp@srcpa.gov>
2024-10-08 09:06:16 -0500

    Disable draggable since it's not done

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -39,7 +39,7 @@ export default function Index() {
 				<RadioGroupCard />
 				<DialogCard />
 				<AccordionCard />
-				<DraggableListCard />
+				{/* <DraggableListCard /> */}
 				<PopoverCard />
 				<CommandCard />
 			</div>
