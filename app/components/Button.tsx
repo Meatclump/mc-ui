fc66ff3c1dfe1325b3adcc46f1019c8c58bc1bfe
Henrik Persson <henrikp@srcpa.gov>
2024-10-10 12:10:04 -0500

    Support asChild in Button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,21 @@
 import React from "react"
 import { cn } from "~/lib/cn"
+import { Slot } from "@radix-ui/react-slot"
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	variant?: "default" | "contrast"
+	variant?: "default" | "contrast",
+	asChild?: boolean
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({variant, className, children, ...props}, forwardedRef) => {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({variant, className, children, asChild, ...props}, forwardedRef) => {
 	const variants: {[key: string]: string} = {
 		"default": "px-2 py-1 border border-slate-400 text-slate-800 bg-white hover:border-slate-600 active:bg-slate-100 dark:border-slate-500 dark:text-slate-300 dark:bg-slate-800 rounded-md dark:hover:text-white dark:hover:bg-slate-900 dark:active:bg-slate-950 dark:hover:border-slate-400",
 		"contrast": "px-2 py-1 rounded-md border border-transparent bg-slate-800 text-white hover:bg-slate-900 active:bg-slate-950 dark:bg-slate-200 dark:text-slate-800 dark:hover:bg-white dark:active:bg-slate-200"
 	}
+	const Comp = asChild ? Slot : "button"
 
 	return (
-		<button
+		<Comp
 			ref={forwardedRef}
 			className={cn(
 				variants[variant ?? "default"],
@@ -22,7 +25,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({variant, class
 			{...props}
 		>
 			{children}
-		</button>
+		</Comp>
 	)
 })
 
